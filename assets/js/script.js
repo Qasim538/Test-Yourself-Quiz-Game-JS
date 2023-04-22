b298a2d121b7901f4c0020f99e2cbcd4fa8e8b7b
Qasim Malik <qasimmalik@qasims-mbp.home>
2023-04-22 13:17:43 +0100

    just added all IDs to the js to start coding

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+// All Id are here
+
+var highScoreLink = document.getElementById('highscores');
+var timer = document.getElementById('time');
+var startQuizBtn = document.getElementById('start-quiz-btn');
+var questionDisplay = document.getElementById('question-title');
+var answerChoices = document.getElementById('choices');
+var finalScore = document.getElementById('final-score');
+var initialInput = document.getElementById('initials');
+var submitIniBtn = document.getElementById('submit');
+var highScores = document.getElementById('highscores');
+var clearHighScore = document.getElementById('clear');
+
