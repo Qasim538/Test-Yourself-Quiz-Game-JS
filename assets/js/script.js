fd59a35c10070caaadc6541a7858b5a952e83a19
Qasim Malik <qasimmalik@qasims-mbp.home>
2023-04-24 17:33:44 +0100

    updated javascript and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
+// initialize all important elements
 
-// All Id are here
+var main = document.getElementsByTagName('main')[0]
+var viewHighscoreLink = document.getElementById('view_highscore_link')
+var timeDisplay = document.getElementById('time_display')
+var startQuizButton = document.getElementById('start_quiz_button')
+var questionNumbersBox = document.getElementById('question_numbers_box')
+var questionDisplay = document.getElementById('question_display')
+var answersList = document.getElementById('answer_list')
+var answerFeedback = document.getElementById('feedback')
+var scoreDisplay = document.getElementById('score_display')
+var initialsInput = document.getElementById('initials_input')
+var submitInitialsButton = document.getElementById('submit_initials_button')
+var highscoreList = document.getElementById('highscore_list')
+var goToStartingPageButton = document.getElementById('go_to_starting_page_button')
+var clearHighscoresButton = document.getElementById('clear_highscores_button')
 
-var highScoreLink = document.getElementById('highscores');
-var timer = document.getElementById('time');
-var startQuizBtn = document.getElementById('start-quiz-btn');
-var questionDisplay = document.getElementById('question-title');
-var answerChoices = document.getElementById('choices');
-var finalScore = document.getElementById('final-score');
-var initialInput = document.getElementById('initials');
-var submitIniBtn = document.getElementById('submit');
-var highScores = document.getElementById('highscores');
-var clearHighScore = document.getElementById('clear');
-
-// Questions to be asked as a lits  with correct answers strings
+// Questions with answers
 
 const questions = [
     {
@@ -55,78 +58,247 @@ const questions = [
         'question': 'Content information that appears between the oppening and closing ______ tags will show up in a browser view.',
         'answers': ['body','html','head','table'],
         'correct_index': 0
-    }, {
-        'question': 'What tag element will display a line across the screen?',
-        'answers': ['br','a','hr','line'],
-        'correct_index': 2
-    }, {
-        'question': 'In a/an ______ list numbers and roman numerals can appear.',
-        'answers': ['Unordered','Ordered','Definition','Straight line'],
-        'correct_index': 1
-    }, {
-        'question': 'This is a language used for describing the look and formatting of a document written in markup language.',
-        'answers': ['HTML','CSS','JSON','XML'],
-        'correct_index': 1
-    }, {
-        'question': 'There are ______ characters in the hexidecimal color code combining color shades of red, green, and blue.',
-        'answers': ['4','5','6','7'],
-        'correct_index': 2
-    }, {
-        'question': 'Which HTML tag is used to communicate with search engines and describe the Web page\'s content?',
-        'answers': ['meta','html','body','head'],
-        'correct_index': 0
-    }, {
-        'question': 'Which term describes the skeletal layout of a Web page, without colors or graphics? ',
-        'answers': ['A wireframe','Mind map','Template','Goals'],
-        'correct_index': 0
-    }
-
+    }, 
 ]
 
 // score tracking variables
 
-const startingTime = 15;
-var remainingTime;
-var timer;
-var score;
+const startingTime = questions.length * 8 
+const timePenalty = 10 
+var remainingTime 
+var timer 
+var score 
 
-// Event Listeners to all
+/** Set up the pages and get the quiz ready. */
 
 function init() {
-    startQuizBtn.addEventListener('click', event => {
-        event.preventDefault();
+
+    // Add all of the event listeners
+    startQuizButton.addEventListener('click', event => {
+        event.preventDefault()
         displayQuestionPage()
     })
-    answerChoices.addEventListener('click', function(event){
+    answersList.addEventListener('click', function(event) {
         event.preventDefault()
-        if(event.target.matches('button')) {
+        if (event.target.matches('button')) {
             var button = event.target
-            if (button.classList.cointain('correct')) {
-                answerChoices.textContent+"correct!";
+            if (button.classList.contains('correct')) {
+                answerFeedback.textContent = "Correct!"
+                questionNumbersBox.children[nextQuestionIndex - 1].classList.add('correct')
                 score++
             } else {
-                answerChoices.textContent = "Wrong!";
+                answerFeedback.textContent = "Wrong!"
+                questionNumbersBox.children[nextQuestionIndex - 1].classList.add('wrong')
+                remainingTime -= timePenalty
             }
-
-            if (remainingTime > 0) dispplayNextQuestion()
+            if (remainingTime > 0) displayNextQuestion()
             else displayGetNamePage()
         }
     })
-
-    submitIniBtn.addEventListener('click', event =>{
+    submitInitialsButton.addEventListener('click', event => {
         event.preventDefault()
-        let initials = initialInput.ariaValueMax.toUpperCase()
+        let initials = initialsInput.value.toUpperCase()
         if (initials) {
-            let highScores = JSON.parese(localStorage.getItem('highscores')) || []
-
+            let highscores = JSON.parse(localStorage.getItem('highscores')) || []
+            
             timestamp = Date.now()
-            highScores.push ({
+            highscores.push({
                 'timestamp': timestamp,
                 'score': score,
                 'initials': initials,
                 'timeRemaining': remainingTime
             })
+            
+            highscores = highscores.sort((a, b) => {
+                if (a.score != b.score) return b.score - a.score
+                if (a.timeRemaining != b.timeRemaining) return b.timeRemaining - a.timeRemaining
+                if (a.timestamp != b.timestamp) return a.timestamp - b.timestamp
+                return 0
+            })
+
+            localStorage.setItem('highscores', JSON.stringify(highscores))
+            
+            displayHighscorePage()
+            initialsInput.value = ""
         }
     })
-
+    goToStartingPageButton.addEventListener('click', event => {
+        event.preventDefault()
+        displayStartingPage()
+    })
+    clearHighscoresButton.addEventListener('click', event => {
+        var confirmed = confirm("You are about to clear all of your highscores. Would you like to continue?")
+        if (confirmed) {
+            event.preventDefault()
+            localStorage.setItem('highscores', "[]")
+            displayHighscorePage()
+        }
+    })
+    viewHighscoreLink.addEventListener('click', event => {
+        event.preventDefault()
+        displayHighscorePage()
+    })
+    
+    // display the starting page
+    
+    displayStartingPage()
 }
+
+/** Hide all of the pages except for the one with given id.
+ * */
+function displayPage(id) {
+    main.querySelectorAll('.page').forEach(page => {
+        if (page.id == id) {
+            page.classList.remove('hidden')
+        } else {
+            page.classList.add('hidden')
+        }
+    })
+    return 4
+}
+
+/** Display the starting page. */
+function displayStartingPage() {
+    displayPage('starting_page')
+    
+    clearInterval(timer)
+    remainingTime = 0
+    timeDisplay.textContent = formatSeconds(remainingTime)
+}
+
+var nextQuestionIndex // The index of question currently being displayed to the user 
+var randomizedQuestions // A randomly sorted clone of the questions array
+
+/** Display the questions page. */
+function displayQuestionPage() {
+    displayPage('question_page')
+
+    // setup the question numbers
+
+    for (let i = 0; i < questions.length; i++) {
+        const element = questions[i];
+        var el = document.createElement('span')
+        el.textContent = i + 1
+        questionNumbersBox.appendChild(el)
+    }
+
+    // create a randomly sorted clone of the questions array to use for this quiz
+    randomizedQuestions = randomizeArray(questions)
+
+    // reset the values to back to their defaults
+    nextQuestionIndex = 0
+    score = 0
+
+    // start the timer
+    startTimer()
+
+    // setup the first question
+    displayNextQuestion()
+}
+
+/** Display the next question. */
+function displayNextQuestion() {
+    if (nextQuestionIndex < questions.length) {
+        // get the question and answers from the 
+        const question = randomizedQuestions[nextQuestionIndex].question
+        const answers = randomizedQuestions[nextQuestionIndex].answers
+        const randomizedAnswers = randomizeArray(answers)
+        const correctAnswer = answers[randomizedQuestions[nextQuestionIndex].correct_index]
+        
+        questionDisplay.textContent = question
+        answersList.innerHTML = ""
+        answerFeedback.textContent = ""
+
+        for (let i = 0; i < randomizedAnswers.length; i++) {
+            let answer = randomizedAnswers[i]
+            let button = document.createElement("button")
+            button.classList.add('answer')
+            if (answer == correctAnswer)
+                button.classList.add('correct')
+            button.textContent = `${i + 1}. ${answer}`
+            answersList.appendChild(button)
+        }
+
+        nextQuestionIndex++
+    } else {
+        clearInterval(timer)
+        displayGetNamePage()
+    }
+}
+
+/** Display the get name page. */
+function displayGetNamePage() {
+    displayPage('get_name_page')
+    if (remainingTime < 0) remainingTime = 0
+    timeDisplay.textContent = formatSeconds(remainingTime)
+    scoreDisplay.textContent = score
+}
+
+/** Display the highscore page. */
+function displayHighscorePage() {
+    displayPage('highscore_page')
+    questionNumbersBox.innerHTML = ""
+
+    highscoreList.innerHTML = ""
+
+    clearInterval(timer)
+
+    let highscores = JSON.parse(localStorage.getItem('highscores'))
+    
+    let i = 0
+    for (const key in highscores) {
+        i++
+        let highscore = highscores[key]
+        var el = document.createElement('div')
+        let initials = highscore.initials.padEnd(3, ' ')
+        let playerScore = highscore.score.toString().padStart(3, ' ')
+        let timeRemaining = formatSeconds(highscore.timeRemaining)
+        el.textContent = `${i}. ${initials} - Score: ${playerScore} - Time: ${timeRemaining}`
+        highscoreList.appendChild(el)
+    }
+}
+
+/** Take any array and return a randomly sorted clone.
+ */
+function randomizeArray(array) {
+    clone = [...array]
+    output = []
+    
+    while (clone.length > 0) {
+        let r = Math.floor(Math.random() * clone.length);
+        let i = clone.splice(r, 1)[0]
+        output.push(i)
+    }
+
+    return output
+}
+
+/** Start the count down timer */
+
+function startTimer() {
+    remainingTime = startingTime
+    timeDisplay.textContent = formatSeconds(remainingTime)
+    
+    timer = setInterval(function() {
+        remainingTime--
+    
+        if (remainingTime < 0) {
+            clearInterval(timer)
+            displayGetNamePage()
+        } else {
+            timeDisplay.textContent = formatSeconds(remainingTime)
+        }
+    
+    }, 1000)
+}
+
+/** Convert a given number of seconds to a 'M or S' format
+
+ */
+function formatSeconds(seconds) {
+    let m = Math.floor(seconds / 60).toString().padStart(2, ' ')
+    let s = (seconds % 60).toString().padStart(2, '0')
+    return `${m}:${s}`
+}
+
+init()
